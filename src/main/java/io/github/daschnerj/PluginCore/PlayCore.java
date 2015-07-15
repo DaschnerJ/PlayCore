1eed0cc2c22c89ebc72d299d1a0a16151c1a5ceb
Mr. Daschner <Daschner02@hotmail.com>
2015-07-15 16:56:57 -0400

    Added in class variables.

--- src/main/java/io/github/daschnerj/PluginCore/PlayCore.java
+++ src/main/java/io/github/daschnerj/PluginCore/PlayCore.java
@@ -1,15 +1,29 @@
 package io.github.daschnerj.PluginCore;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import io.github.daschnerj.PluginCore.functions.Chests;
+import io.github.daschnerj.PluginCore.functions.Enchant;
+import io.github.daschnerj.PluginCore.functions.Locations;
+import io.github.daschnerj.PluginCore.functions.Name;
+import io.github.daschnerj.PluginCore.functions.Probability;
+import io.github.daschnerj.PluginCore.functions.Utility;
+
 public class PlayCore extends JavaPlugin 
 {
+	PlayCore instance;
+	
+	public static Chests c = new Chests();
+	public static Enchant e = new Enchant();
+	public static Locations l = new Locations();
+	public static Name n = new Name();
+	public static Probability p = new Probability();
+	public static Utility u = new Utility();
 	
 	@Override
 	public void onEnable() 
 	{
-		
-		getLogger().info("PlayCore has been enabled!");
-		
+		instance = this;
+		getLogger().info("PlayCore has been enabled!");	
 	}
  
 	@Override
