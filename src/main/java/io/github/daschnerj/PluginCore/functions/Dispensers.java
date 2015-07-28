3b62e142e300d9d8990b9d6dd61becf5f76a6575
Damien <Damien@10.14.200.172>
2015-07-27 17:40:52 -0700

    Created functions for basic and advanced manipulation of Dispensers.

--- src/main/java/io/github/daschnerj/PluginCore/functions/Dispensers.java
+++ src/main/java/io/github/daschnerj/PluginCore/functions/Dispensers.java
@@ -1,5 +1,168 @@
 package io.github.daschnerj.PluginCore.functions;
 
+import java.util.HashMap;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Dispenser;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
 public class Dispensers {
+	
+	/**
+	 * Creates a Dispenser at the given location.
+	 * 
+	 * @param Location - Location to create a Dispenser.
+	 * @return The Dispenser created. Once created, it cannot be guaranteed that it will remain a dispenser in the future.
+	 */
+	
+	public Dispenser createDispenser(Location Location)
+	{
+		
+		Location.getBlock().setType(Material.DISPENSER);
+		
+		Dispenser dispenser = (Dispenser) Location.getBlock().getState();
+		
+		return dispenser;
+		
+	}
+	
+	/**
+	 * Gets the Dispenser at the given location.
+	 * 
+	 * @param Location - The location to look for a Dispenser.
+	 * @return The Dispenser, if found. Otherwise, null. Once gotten, it cannot be guaranteed that it will remain a dispenser in the future.
+	 */
+	
+	public Dispenser getDispenser(Location Location)
+	{
+				
+		if(Location.getBlock().getType().equals(Material.DISPENSER))
+		{
+			
+			Dispenser dispenser = (Dispenser) Location.getBlock().getState();
+			
+			return dispenser;
+			
+		}
+		else
+		{
+			
+			return null;
+			
+		}
+		
+	}
+	
+	/**
+	 * Adds an item to a Dispenser.
+	 * 
+	 * @param Dispenser - Dispenser to add to.
+	 * @param Itemstack - ItemStack to add to the dispenser.
+	 * @return True if item was added without problems, false otherwise.
+	 */
+	
+	public boolean addItem(Dispenser Dispenser, ItemStack Itemstack)
+	{
+		
+		boolean success = false;
+		
+		if(Itemstack != null)
+		{
+			
+			Inventory dispenserInv = Dispenser.getInventory();
+			
+			HashMap<Integer, ItemStack> hashmap = dispenserInv.addItem(Itemstack);
+			
+			if(hashmap.isEmpty()) {
+				
+				success = true;
+				
+			}
+			
+		}
+		
+		return success;
+		
+	}
+	
+	/**
+	 * Removes an item to a Dispenser.
+	 * 
+	 * @param Dispenser - Dispenser to add to.
+	 * @param Itemstack - ItemStack to add to the dispenser.
+	 * @return True if item was added without problems, false otherwise.
+	 */
+	
+	public boolean removeItem(Dispenser Dispenser, ItemStack Itemstack)
+	{
+		
+		boolean success = false;
+		
+		if(Itemstack != null)
+		{
+			
+			Inventory dispenserInv = Dispenser.getInventory();
+			
+			HashMap<Integer, ItemStack> hashmap = dispenserInv.removeItem(Itemstack);
+			
+			if(hashmap.isEmpty()) {
+				
+				success = true;
+				
+			}
+			
+		}
+		
+		return success;
+		
+	}
+	
+	/**
+	 * Gets the direction of the Dispenser given.
+	 * 
+	 * @param Dispenser - The Dispenser to get the direction of.
+	 * @return The direction, or if not found correctly -1.
+	 */
+	
+	@SuppressWarnings("deprecation")
+	public byte getDirection(Dispenser Dispenser) {
+		
+		byte direction = -1;
+		
+		if(Dispenser.getBlock().getData() >= 0 && Dispenser.getBlock().getData() <= 5) {
+			
+			direction = Dispenser.getBlock().getData();
+			
+		}
+		
+		if(Dispenser.getBlock().getData() >= 8 && Dispenser.getBlock().getData() <= 13) {
+			
+			direction = (byte) (Dispenser.getBlock().getData() - 8);
+			
+		}
+		
+		return direction;
+		
+	}
+	
+	/**
+	 * Set the direction of the Dispenser given.
+	 * 
+	 * @param Direction - Direction to set the dispenser to. 0 - Negative Y, 1 - Positive Y, 2 - Negative Z, 3 - Positive Z, 4 - Negative X, 5 - Positive X.
+	 * @param Dispenser - The Dispenser to set the direction of.
+	 */
+	
+	@SuppressWarnings("deprecation")
+	public void setDirection(int Direction, Dispenser Dispenser) {
+		
+		if(Direction >= 0 && Direction <= 5) {
+			
+			Dispenser.getBlock().setData((byte) Direction);
+			
+		}
+				
+	}
 
 }
--- src/main/java/io/github/daschnerj/PluginCore/specialEffects/DispenserFX.java
+++ src/main/java/io/github/daschnerj/PluginCore/specialEffects/DispenserFX.java
@@ -1,5 +1,153 @@
 package io.github.daschnerj.PluginCore.specialEffects;
 
+import org.bukkit.block.Dispenser;
+import org.bukkit.inventory.ItemStack;
+
 public class DispenserFX {
+	
+	/**
+	 * A neat function that helps you to create a custom "craft" of an item. How you call it and testing is up to you.
+	 * 
+	 * @param dispenser - Dispenser to remove items from.
+	 * @param slot1 - What to remove from slot 1. Can be null only if its corresponding delete boolean is false.
+	 * @param slot2 - What to remove from slot 2. Can be null only if its corresponding delete boolean is false.
+	 * @param slot3 - What to remove from slot 3. Can be null only if its corresponding delete boolean is false.
+	 * @param slot4 - What to remove from slot 4. Can be null only if its corresponding delete boolean is false.
+	 * @param slot5 - What to remove from slot 5. Can be null only if its corresponding delete boolean is false.
+	 * @param slot6 - What to remove from slot 6. Can be null only if its corresponding delete boolean is false.
+	 * @param slot7 - What to remove from slot 7. Can be null only if its corresponding delete boolean is false.
+	 * @param slot8 - What to remove from slot 8. Can be null only if its corresponding delete boolean is false.
+	 * @param slot9 - What to remove from slot 9. Can be null only if its corresponding delete boolean is false.
+	 * @param delete1 - Whether or not to remove an item from slot 1.
+	 * @param delete2 - Whether or not to remove an item from slot 2.
+	 * @param delete3 - Whether or not to remove an item from slot 3.
+	 * @param delete4 - Whether or not to remove an item from slot 4.
+	 * @param delete5 - Whether or not to remove an item from slot 5.
+	 * @param delete6 - Whether or not to remove an item from slot 6.
+	 * @param delete7 - Whether or not to remove an item from slot 7.
+	 * @param delete8 - Whether or not to remove an item from slot 8.
+	 * @param delete9 - Whether or not to remove an item from slot 9.
+	 */
+	
+	public static void removeDispenserItems(Dispenser dispenser, ItemStack slot1, ItemStack slot2, ItemStack slot3, ItemStack slot4, ItemStack slot5, ItemStack slot6, ItemStack slot7, ItemStack slot8, ItemStack slot9, Boolean delete1, Boolean delete2, Boolean delete3, Boolean delete4, Boolean delete5, Boolean delete6, Boolean delete7, Boolean delete8, Boolean delete9){
+		
+		if(delete1 == true){
+		if(slot1.getAmount() > 1) {
+			
+			slot1.setAmount(slot1.getAmount() - 1);
+			
+		}
+		else{
+											
+			dispenser.getInventory().removeItem(slot1);
+			
+		}
+		}
+		
+		if(delete2 == true){
+		if(slot2.getAmount() > 1) {
+			
+			slot2.setAmount(slot2.getAmount() - 1);
+			
+		}
+		else{
+			
+			dispenser.getInventory().removeItem(slot2);
+			
+		}
+		}
+		
+		if(delete3 == true){
+		if(slot3.getAmount() > 1) {
+			
+			slot3.setAmount(slot3.getAmount() - 1);
+			
+		}
+		else{
+			
+			dispenser.getInventory().removeItem(slot3);
+			
+		}
+		}
+		
+		if(delete4 == true){
+		if(slot4.getAmount() > 1) {
+			
+			slot4.setAmount(slot4.getAmount() - 1);
+			
+		}
+		else{
+			
+			dispenser.getInventory().removeItem(slot4);
+			
+		}
+		}
+		
+		if(delete5 == true){
+		if(slot5.getAmount() > 1) {
+			
+			slot5.setAmount(slot5.getAmount() - 1);
+			
+		}
+		else{
+			
+			dispenser.getInventory().removeItem(slot5);
+			
+		}
+		}
+		
+		if(delete6 == true){
+		if(slot6.getAmount() > 1) {
+			
+			slot6.setAmount(slot6.getAmount() - 1);
+			
+		}
+		else{
+			
+			dispenser.getInventory().removeItem(slot6);
+			
+		}
+		}
+		
+		if(delete7 == true){
+		if(slot7.getAmount() > 1) {
+			
+			slot7.setAmount(slot7.getAmount() - 1);
+			
+		}
+		else{
+			
+			dispenser.getInventory().removeItem(slot7);
+			
+		}
+		}
+		
+		if(delete8 == true){
+		if(slot8.getAmount() > 1) {
+			
+			slot8.setAmount(slot8.getAmount() - 1);
+			
+		}
+		else{
+			
+			dispenser.getInventory().removeItem(slot8);
+			
+		}
+		}
+		
+		if(delete9 == true){
+		if(slot9.getAmount() > 1) {
+			
+			slot9.setAmount(slot9.getAmount() - 1);
+			
+		}
+		else{
+			
+			dispenser.getInventory().removeItem(slot9);
+			
+		}
+		}
+		
+	}
 
 }
